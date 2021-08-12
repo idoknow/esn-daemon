2ebdbfbe3338e4679af374eb27d26f0910bc2e98
Rock Chin <1010553892@qq.com>
2021-08-12 16:19:08 +0800

    Auto create config file

--- src/main/main.go
+++ src/main/main.go
@@ -4,6 +4,7 @@ import (
 	"esnd/src/db"
 	"esnd/src/service"
 	"esnd/src/util"
+	"os"
 	"strconv"
 	"sync"
 )
@@ -12,13 +13,69 @@ var Cfg *util.Config
 var Service *service.NetService
 var wg sync.WaitGroup
 
+var configDefault = `[server]
+service.port=3003
+
+[mysql]
+db.user=esnd
+db.addr=127.0.0.1:3306
+db.pass=changeMe
+db.database=esnd
+
+[admin]
+root.mask=changeMe`
+
 func main() {
-	Cfg, err := util.LoadConfig(".esnd/esnd.conf")
+	configName := "config/esnd.conf"
+	if len(os.Args) == 1 {
+
+		fileExist, err := PathExists("config/esnd.conf")
+		if err != nil {
+			panic(err)
+		}
+		if !fileExist {
+			util.SaySub("Main", "Config file not found,creating './config/esnd.conf' and exit.")
+
+			exist, err := PathExists("config")
+			if err != nil {
+				panic(err)
+			}
+			if !exist {
+				os.MkdirAll("config", os.ModePerm)
+			}
+			//create
+			util.Sayln(configDefault)
+
+			err = os.WriteFile("config/esnd.conf", []byte(configDefault), os.ModePerm)
+
+			if err != nil {
+				util.SaySub("Main", "Cannot create config file")
+				panic(err)
+			}
+
+			util.SaySub("Main", "Config file created,please edit it,then restart program.")
+			os.Exit(0)
+		}
+		configName = "config/esnd.conf"
+	} else {
+		configName = os.Args[1]
+	}
+
+	exist, err := PathExists(configName)
 	if err != nil {
 		panic(err)
 	}
+	if !exist {
+		util.SaySub("Main", "Config file:"+configName+" not found.")
+		os.Exit(1)
+	}
+	util.SaySub("Main", "Loading config file:"+configName)
+	cfg, err := util.LoadConfig(configName)
+	if err != nil {
+		panic(err)
+	}
+	Cfg = cfg
 	util.SaySub("Main", "Config file loaded.")
-
 	err = db.Init(Cfg)
 	if err != nil {
 		panic(err)
@@ -37,4 +94,15 @@ func main() {
 
 	wg.Add(1)
 	wg.Wait()
+}
+
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
